c8e6327b7266e6ae52f8268818943d2b916077a3
Jonathan Stevens <jonathan@videndum.studio>
2020-12-08 23:25:41 +0000

    fix attempt 8

--- packages/release-mastermind/src/contextHandler.ts
+++ packages/release-mastermind/src/contextHandler.ts
@@ -107,7 +107,7 @@ class ContextHandler {
       sha: context.sha,
       action: context.payload.action as string,
       currentVersion,
-      IDNumber,
+      IDNumber: context.payload.pull_request?.id,
       prProps: {
         branch: pr.head.ref,
         creator: pr.user.login,
