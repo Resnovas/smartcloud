9d8dea7ab822b20b8a4a57a22a9f6edf9b0f81de
Jonathan Stevens <jonathan@videndum.studio>
2020-12-11 17:09:44 +0000

    fix(release): project not found

--- extractLabeler.sh
+++ extractLabeler.sh
@@ -6,6 +6,6 @@ yes | cp -R -i packages/release-mastermind/src/api packages/label-mastermind/src
 yes | cp -R -i packages/release-mastermind/src/conditions packages/label-mastermind/src
 yes | cp -R -i packages/release-mastermind/src/utils packages/label-mastermind/src
 yes | cp -R -i packages/release-mastermind/src/contextHandler.ts packages/label-mastermind/src
-yes | cp -R -i packages/release-mastermind/.github/allconfigs.json .github/
+yes | cp -R -i .github/allconfigs.json packages/release-mastermind/.github/
 yes | cp -R -i .github/allconfigs.json packages/label-mastermind/.github/
 yes | cp -R -i .github/allconfigs.json packages/variable-mastermind/.github/
